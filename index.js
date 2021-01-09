e35b1e6f0cb30a1113e733024b2037dbb16b4415
colorv <nexsblim@naver.com>
2021-01-09 23:00:42 +0900

    js-event handlers

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-console.log("warning");
-//string
-const name = "Lim su Hyeok";
-console.log(name);
+const title = document.querySelector("#title");
 
-//Boolean
-const bool = true;
-console.log(bool);
+function handleClick() {
+  title.style.color = "blue";
+}
 
-//number
-const num = 100;
-console.log(num);
-
-//folat
-const float = 1.1;
-console.log(float);
-
-//array
-const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-console.log(daysOfWeek);
-
-//objects
-const info = {
-  name: "LimSuHyeok",
-  age: 31,
-  gender: "male",
-  favMovie: ["Along the gods", "Oldboy", "Tenet"],
-  favFood: [
-    { name: "Kimchi", fatty: false },
-    { name: "cheese burger", fatty: true },
-  ],
-};
-console.log(info.gender);
-info.gender = "female";
-console.log(info.gender);
-
-console.log(info);
+title.addEventListener("click", handleClick);
