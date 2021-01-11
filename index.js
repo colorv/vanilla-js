f89565d920e4af2ac0036611fefe722f2090ec48
colorv <nexsblim@naver.com>
2021-01-11 21:17:37 +0900

    JS 2.5~2.7 practice

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  title.style.color = "blue";
+  title.classList.toggle(CLICKED_CLASS);
+
+  /* 밑에 동작들을 토글을 이용해서 한 줄로 표현 가능 
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }
+  */
 }
 
-title.addEventListener("click", handleClick);
+function init() {
+  title.addEventListener("click", handleClick);
+}
+
+init();
